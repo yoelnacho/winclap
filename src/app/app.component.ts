51cd10390ff4336cd1687f874f53bfaa9ef21d70
yoelnacho <yoelnacho@gmail.com>
2017-01-24 14:14:06 -0300

    Assets y optimización de imágenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+    title:string = 'winclap works!';
+    footer:string = '2017';
 }
